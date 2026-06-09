cf67415e0cadef7e9777570a0fd7663e4eff430c
Rishav Raj <rishav.24b1541080@abes.ac.in>
2026-06-09 20:53:05 +0530

    Refactor User Model

--- models/user.js
+++ models/user.js
@@ -1,51 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    leetcodeUsername: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
+const userSchema = new mongoose.Schema(
+    {
+        leetcodeUsername: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        telegramChatId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        lastSolvedCount: {
+            type: Number,
+            default: 0
+        },
+        isActive: {
+            type: Boolean,
+            default: true
+        }
     },
+    {
+        timestamps: true
+    }
+);
 
-    telegramChatId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-
-    lastSolvedCount: {
-        type: Number,
-        required: true,
-    },
-
-    lastCheckDate: {
-        type: Date,
-        default: null,
-    },
-
-    reminderLevel: {
-        type: Number,
-        default: 0,
-    },
-
-    lastReminderSentAt: {
-        type: Date,
-        default: null,
-    },
-
-    isActive: {
-        type: Boolean,
-        default: true,
-    },
-
-    registeredAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
-
-userSchema.index({ isActive: 1 });
-
-module.exports = userSchema
+module.exports = mongoose.model('User', userSchema);
