a3c68e8dfb40b29b4f31f71155bfb44c04c9496a
Rishav Raj <rishav.24b1541080@abes.ac.in>
2026-06-13 22:24:06 +0530

    Production bug fix

--- cron/reminderCron.js
+++ cron/reminderCron.js
@@ -6,8 +6,7 @@ const {
 console.log("Reminder cron loaded");
 
 cron.schedule(
-    // "30 16 * * *",
-    "53 16 * * * ",
+    "30 16 * * *",
     async () => {
         console.log(
             "Running reminder job at 16:30 UTC"
